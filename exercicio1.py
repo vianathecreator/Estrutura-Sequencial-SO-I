13d869684bdec74f97e4f024e3430f60b261a48c
Felipe Viana <felipeviana2501@gmail.com>
2026-03-02 16:31:33 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,11 @@
+##Declaraçao de variavel
+lado:int
+area:int
+
+#inicio
+lado = int(input("Digite o lado do quadrado: "))
+area = lado * lado
+print(area)
+
+
+#fim
---
+++ exercicio2.py
@@ -0,0 +1,11 @@
+##Declaraçao de variavel
+antigo:int
+novo:int
+
+#inicio
+antigo = int(input("Digite o seu salário: "))
+novo = (antigo * 1.15)
+print(novo)
+
+
+#fim
