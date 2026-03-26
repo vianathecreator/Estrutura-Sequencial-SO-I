9230be2f113967bc3b6cd187d70980d782e17884
Felipe Viana <felipeviana2501@gmail.com>
2026-03-25 21:54:32 -0300

    Add files via upload

---
+++ exercicio23.py
@@ -0,0 +1,9 @@
+valor1=int(input("Digite o primeiro valor: "))
+valor2=int(input("Digite o segundo valor: "))
+valor3=int(input("Digite o terceiro valor: "))
+valor4=int(input("Digite o quarto valor: "))
+
+valores = [valor1, valor2, valor3, valor4]
+valores.sort()
+print(valores)
+
